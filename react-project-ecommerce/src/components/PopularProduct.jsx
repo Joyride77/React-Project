2930aa240e8dc12ebe4b1198fd6a0f02b68f8c23
joyride77 <nfive8@gmail.com>
2023-01-10 11:02:57 +0800

    added footer

--- react-project-ecommerce/src/components/PopularProduct.jsx
+++ react-project-ecommerce/src/components/PopularProduct.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import AliceCarousel from 'react-alice-carousel'
 import { popularData } from '../data/menus'
+// import * as React from 'react';
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
+import StarIcon from '@mui/icons-material/Star';
 
 const PopularProduct = () => {
     const popular = popularData.map(populardata => {
         return (
             <div id='card'>
                 {populardata.page.map(data => {
+                    const value = 3.5;
                     return (
                         <div className="row popular-product p-2">
                             <div className="row product-image">
@@ -17,12 +22,21 @@ const PopularProduct = () => {
                             <div className="row">
                                 <div className="col">
                                     <h6>{data.id}</h6>
-                                    <p>{data.price}</p>
-                                </div>
-                                <div className="col-4 d-flex justify-content-end">
-                                    <img className='shoppy' src={data.shop} alt={data.id} />
                                 </div>
                             </div>
+                            <div className="row">
+                                <div className="col">
+                                    <p>{data.price}</p>
+                                    <Stack spacing={1}>
+                                        <Rating name="size-small" defaultValue={2} size="small" />
+                                    </Stack>
+                                </div>
+                                <div className="col d-flex justify-content-end">
+                                    <img className='shoppy' src={data.shop} alt={data.id} />
+                                </div>
+
+                            </div>
+
                         </div>
                     )
                 })}
