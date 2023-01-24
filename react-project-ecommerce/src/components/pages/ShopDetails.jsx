23865751f9dc8234ae639675bbf73a13cd913578
joyride77 <nfive8@gmail.com>
2023-01-24 09:04:30 +0800

    add

--- react-project-ecommerce/src/components/pages/ShopDetails.jsx
+++ react-project-ecommerce/src/components/pages/ShopDetails.jsx
@@ -5,7 +5,23 @@ const ShopDetails = (props) => {
     const shoplist = props.shopList.map((data, index) => {
         return (
             <div>
-
+                <div className="col">
+                    <div className="row">
+                        <div className="col">
+                            <img src={data.productImage} alt={data.productName} />
+                        </div>
+                        <div className="col">
+                            <h5>{data.productName}</h5>
+                            <h5>Color: Black</h5>
+                            <h5>Size: 30</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <h5>{data.productPrice}</h5>
+                </div>
+                <div className="col">1</div>
+                <div className="col">2</div>
             </div>
         )
     })
@@ -19,10 +35,13 @@ const ShopDetails = (props) => {
                         <div className="col"><h5>Quantity</h5></div>
                         <div className="col"><h5>Subtotal</h5></div>
                     </div>
+                    <div className="row">
+                        {shoplist}
+                    </div>
                 </div>
                 <div className="col"></div>
             </div>
-            {shoplist}
+
         </div>
     )
 }
